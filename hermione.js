d76bc499ef3b648b5ff1f488a31121abf6e73fc0
decamun <decamun@gmail.com>
2014-09-13 13:45:33 -0400

    sorts by score now

--- hermione.js
+++ hermione.js
@@ -4,38 +4,26 @@
 Questions = new Mongo.Collection("questions");
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 100) ;
-
-  //this returns the number of times something has been clicked
-  Template.questionBoxTemplate.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
-
-
   //runs when someone clicks the button next to the question box
   Template.questionBoxTemplate.events({
     'click button': function () {
       // increment the counter when button is clicked
       //alert(document.getElementById("questionBox").value); // "something something";// + counter + " times";
       var questionText = document.getElementById("questionBox").value;
-      Questions.insert({text: questionText, upvotes: 1, downvotes: 0});
-      Session.set("counter", Session.get("counter") + 1);
+      Questions.insert({text: questionText, upvotes: 1, downvotes: 0, score: 1});
     }
   });
 
   //runs to sort the list of questions and return them somehow <- (magic)
   Template.questionBoardTemplate.questions = function () {
-    return Questions.find({}, {sort: {upvotes: -1, text: 1}}); //this is the magic
+    return Questions.find({}, {sort: {score: -1, text: 1}}); //this is the magic
   };
 
   Template.questionTemplate.events({
       'click #upButton': function () {
       //when the button is clicked or something. Do things here.
-      Questions.update(this._id, {$inc: {upvotes: 1}})
-
+      Questions.update(this._id, {$inc: {upvotes: 1}});
+      Questions.update(this._id, {$inc: {score: 1}});
 
       //some quick code to uncomment if you want to delete things:
       //Questions.remove(this._id);
@@ -45,20 +33,12 @@ if (Meteor.isClient) {
   Template.questionTemplate.events({
       'click #downButton': function () {
       //when the button is clicked or something. Do things here.
-      Questions.update(this._id, {$inc: {downvotes: 1}})
+      Questions.update(this._id, {$inc: {downvotes: 1}});
+      Questions.update(this._id, {$inc: {score: -1}});
 
 
       //some quick code to uncomment if you want to delete things:
       //Questions.remove(this._id);
-    }
-  });
-
-
-  //returns the number of upvotes subtracted from the number of downvotes.
-  Template.questionTemplate.helpers({
-    score: function () {
-      var question = Questions.findOne(this._id);
-      return question.upvotes - question.downvotes;
     }
   });
 }
@@ -67,6 +47,8 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
-    Questions.insert({text: "Lorem ipsem blah blah blah", upvotes: 1, downvotes: 0});
+    if(Questions.find().count === 0) {
+      Questions.insert({text: "This is an example question. Ask your own question by writing in the box below.", upvotes: 1, downvotes: 0, score: 1});
+    }
   });
 }
