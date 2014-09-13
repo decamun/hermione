4c7a18258ede164fe938905bc57adff63cd9c470
decamun <decamun@gmail.com>
2014-09-13 15:40:37 -0400

    this works but the css is really screwed up

--- hermione.js
+++ hermione.js
@@ -5,9 +5,6 @@ Questions = new Mongo.Collection("questions");
 
 if (Meteor.isClient) {
 
-  // counter starts at 0
-  Session.setDefault("counter", 0);
-=======
   //runs when someone clicks the button next to the question box
   Template.questionBoxTemplate.events({
     'click button': function () {
@@ -22,7 +19,6 @@ if (Meteor.isClient) {
   Template.questionBoardTemplate.questions = function () {
     return Questions.find({}, {sort: {score: -1, text: 1}}); //this is the magic
   };
->>>>>>> FETCH_HEAD
 
   Template.questionTemplate.events({
       'click #upButton': function () {
@@ -35,14 +31,6 @@ if (Meteor.isClient) {
     }
   });
 
-<<<<<<< HEAD
-  Template.hello.events({
-    'click': function () {
-
-      // increment the counter when Submit button is clicked
-      document.getElementById("Submit").onclick = function () { Session.set("counter", Session.get("counter") + 1); };
-      
-=======
   Template.questionTemplate.events({
       'click #downButton': function () {
       //when the button is clicked or something. Do things here.
@@ -59,7 +47,6 @@ if (Meteor.isClient) {
 
       //some quick code to uncomment if you want to delete things:
       //Questions.remove(this._id);
->>>>>>> FETCH_HEAD
     }
   });
 
@@ -71,7 +58,7 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
-    if(Questions.find().count < 5) {
+    if(Questions.find().count < 1) {
       Questions.insert({text: "This is an example question. Ask your own question by writing in the box below.", upvotes: 1, downvotes: 0, score: 1});
     }
   });
