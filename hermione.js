ccc55378e436275f5d6b1637cfb6f02941283da9
decamun <decamun@gmail.com>
2014-09-13 11:53:33 -0400

    Basic MongoDB Functionality

--- hermione.js
+++ hermione.js
@@ -1,26 +1,55 @@
+
+//create dynamic list of questions
+
+Questions = new Mongo.Collection("questions");
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault("counter", 100) ;
 
-  Template.hello.helpers({
+  Template.questionBoxTemplate.helpers({
     counter: function () {
       return Session.get("counter");
     }
   });
 
-  Template.hello.events({
+  Template.questionBoxTemplate.events({
     'click button': function () {
 
       // increment the counter when button is clicked
       //alert(document.getElementById("questionBox").value); // "something something";// + counter + " times";
-      document.getElementById("questionText").innerHTML = document.getElementById("questionText").innerHTML + "<p>" + document.getElementById("questionBox").value + "</p>";
+      var questionText = document.getElementById("questionBox").value;
+      Questions.insert({text: "" + questionText, upvotes: 1});
       Session.set("counter", Session.get("counter") + 1);
     }
   });
+
+
+  Template.questionBoardTemplate.questions = function () {
+    return Questions.find({}, {sort: {text: -1, upvotes: 1}});
+  };
+
+  Template.questionTemplate.events({
+    'click button': function () {
+      //when the button is clicked or something. Do things here
+    }
+  });
+
+  // Template.questionTemplate.text = function() {
+  //   //return the text for current question
+  //   return "fuck";
+  // };
+
+  // Template.questionTemplate.upvotes = function() {
+  //   //return the number of upvotes for current question
+  //   return -3;
+  // };
 }
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    Questions.insert({text: "Lorem ipsem blah blah blah", upvotes: 1});
   });
 }
